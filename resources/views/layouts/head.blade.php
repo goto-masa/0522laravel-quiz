ce87f4752e096bca35baaff2a8df561bdb385386
goto-masa <bugright.9w1v2.510@gmail.com>
2022-05-29 15:00:10 +0900

    認証周りを調整

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -2,8 +2,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>laravelquiz_app</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>{{ config('app.name', 'Laravel') }}</title>
     <link rel="stylesheet" href="{{asset('/css/styles.css')}}">
+
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
     <script src="{{ asset('js/app.js') }}" defer></script>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
   </head>
